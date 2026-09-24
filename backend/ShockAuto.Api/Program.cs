85b94c66c35583fa21f3f706a7a39bd5df451385
Airshock72 <gelagela2222@gmail.com>
2026-09-24 17:04:38 +0400

    Rename solution file and update Program.cs for ASP.NET Core setup with CORS and OpenAPI integration

--- backend/ShockAuto.Api/Program.cs
+++ backend/ShockAuto.Api/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+var app = builder.Build();
+if (!app.Environment.IsProduction())
+{
+    app.UseCors("AllowFrontend");
+    app.MapOpenApi();
+    app.MapScalarApiReference("/swagger");
+}
+
+app.UseRateLimiter();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
